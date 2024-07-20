5f0ded29dc29519b71b427db69a4e510aceec05d
Charu <charumishra5673@gmail.com>
2024-07-20 20:38:19 +0530

    fibonacci

---
+++ merge-sort-fibonacci/fibonacci.js
@@ -0,0 +1,38 @@
+function fibs(c)
+{
+    if(c==0)
+        return [];
+    if(c==1)
+        return [0];
+    let a=[0,1];
+    let n1=0;
+    let n2=1;
+    c-=2;
+    for(var i=0;i<c;i++)
+    {
+        a.push(n1+n2);
+        n2=n1+n2;
+        n1=n2-n1;
+    }
+    return a;
+}
+
+console.log(fibs(17));
+
+function fibsRec(c)
+{
+    let a=[0,1];
+    if(c==0)
+        return [];
+    if(c==1)
+        return [0];
+    if(c==2)
+        return [0,1];
+    else
+    {
+        a=fibsRec(c-1);
+        return a.concat([a[a.length-1]+a[a.length-2]]);
+    }
+}
+
+console.log(fibsRec(17));
